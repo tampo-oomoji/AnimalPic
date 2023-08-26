42af857cbefda5581435a999f68229efec7cd0ca
EC2 Default User <ec2-user@ip-172-31-39-9.ap-northeast-1.compute.internal>
2023-08-26 10:15:33 +0000

    favorite

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'jquery-ui-rails'
+gem 'jquery-rails'
 gem 'ransack'
 
 gem 'dotenv-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "script.js"
@@ -18,8 +18,8 @@ import 'hammerjs';
 
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
 require('./preview')
 require('jquery')
-require("./slick") 
+require("./slick")
