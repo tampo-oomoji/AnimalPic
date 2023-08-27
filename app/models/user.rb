ad1814f3af4df1e50e4d3d4885438e55215dcad6
EC2 Default User <ec2-user@ip-172-31-39-9.ap-northeast-1.compute.internal>
2023-08-27 13:37:33 +0000

    indent

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   has_many :following_users, through: :followers, source: :followed
   has_many :follower_users, through: :followeds, source: :follower
   has_many :favorites, dependent: :destroy
-  
+
   has_one_attached :profile_image
 
   def follow(user_id)
@@ -40,12 +40,20 @@ class User < ApplicationRecord
   end
 
 
+  # def get_profile_image(width, height)
+  # unless profile_image.attached?
+  #   file_path = Rails.root.join('app/assets/images/no_image.jpg')
+  #   profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  # end
+  # profile_image.variant(resize_to_limit: [width, height]).processed
+
+  # end
   def get_profile_image
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  end
-  profile_image.variant(resize_to_limit: [100, 100]).processed
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
   end
 
 
